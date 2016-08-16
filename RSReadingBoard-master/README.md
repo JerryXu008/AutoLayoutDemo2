2f1d9b0cf006fe66a59f8785fa6f17c128daeb2c
JerryXu008 <song@song-Mac-mini.local>
2016-08-16 14:24:39 +0800

    f
    f

---
+++ RSReadingBoard-master/README.md
@@ -0,0 +1,49 @@
+RSReadingBoard
+==============
+
+RSReadingBoard is a ZAKER alike news/article reading board for iOS7 and demonstrates how to use Auto Layout. For iPhone only UIInterfaceOrientationMaskPortrait is supported and for iPad UIInterfaceOrientationMaskAllButUpsideDown is supported.
+
+#持续关注马航事件。
+
+Screen shots
+------------
+
+####iPhone
+
+![ScreenShot](https://github.com/yeahdongcn/RSReadingBoard/blob/master/1.png?raw=true)![ScreenShot](https://github.com/yeahdongcn/RSReadingBoard/blob/master/2.png?raw=true)![ScreenShot](https://github.com/yeahdongcn/RSReadingBoard/blob/master/3.png?raw=true)
+
+####iPad
+
+![ScreenShot](https://github.com/yeahdongcn/RSReadingBoard/blob/master/iPad_landscape.png?raw=true)
+
+Usage
+------------
+1. Import `RSReadingBoard.h` `RSReadingBoard.m` and xib files into your project. 
+2. If you need custom layout go directly modify the xib file but do `NOT` change the relationships between the default controls. For example, you do not want the colour line view, set its width to `0`. 
+3. Import `RSReadingBoard.h` into your code, this header file has been well commented, try change these public properties to achieve your goal.
+
+License
+------------
+    The MIT License (MIT)
+
+    Copyright (c) 2012-2014 P.D.Q.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/yeahdongcn/rsreadingboard/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+
---
+++ RSReadingBoard-master/RSReadingBoardSample/RSReadingBoardSample/RSAppDelegate.m
@@ -0,0 +1,70 @@
+//
+//  RSAppDelegate.m
+//  RSReadingBoardSample
+//
+//  Created by R0CKSTAR on 3/25/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+#import "RSAppDelegate.h"
+
+#import "RSArticle.h"
+
+#import "RSReadingBoard.h"
+
+@implementation RSAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    RSReadingBoard *board = [RSReadingBoard board];
+   
+    
+    RSArticle *article = [RSArticle new];
+    article.image = [UIImage imageNamed:@"image"];
+    article.title = @"标题 刘文倩刘文倩刘文倩刘文倩刘文倩刘文倩";
+    article.source = @"By  河北电台";
+    article.date = @"2014-7-9";
+    article.body = [NSString stringWithContentsOfFile:[[[NSBundle mainBundle] URLForResource:@"Body" withExtension:@"txt"] path] encoding:NSUTF8StringEncoding error:nil];
+    article.color = [UIColor blackColor];
+    article.clips = @[[UIImage imageNamed:@"clip0"],
+                      [UIImage imageNamed:@"clip1"],
+                      ];
+    board.article = article;
+
+   // board.enableImageDragZoomIn=NO;
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = board;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
